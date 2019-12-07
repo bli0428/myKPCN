3bf4f804867ae430fcd03a39849e1417a93f7a7f
dylleealt <dylan_tian@brown.edu>
2019-12-07 17:33:39 -0500

    quick implementation of model, indenting may be off bc vim

--- model.py
+++ model.py
@@ -15,17 +15,34 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 class Denoise(tf.keras.Model):
     def __init__(self):
 	    # reference section 5.2
-	    # 8 or 9 convolution layers of size 100x5x5x21
-	    # no fully connected layers
 	    
-	    # initial input dimensions will change going forward, currently 3, will go up to ~8-10 I think
-	    pass
+	    self.input_patch_size = 65
+	    self.output_patch_size = 21
+	    self.input_channels = 3 # will be up to 27 i believe
+	    self.output_channels = 3
+	    self.num_conv_layers = 9
+	    self.filters = [100, 100, 100, 100, 100, 100, 100, 100, output_patch_size * output_patch_size] # for kpcn, otherwise final layer size is output channels
+	    self.kernel_sizes = [5, 5, 5, 5, 5, 5, 5, 5, 5]
 
+	    assert(len(self.filters) == self.num_conv_layers)
+	    assert(len(self.kernel_sizes) == self.num_conv_layers)
+
+	    self.kernels = []
+	    
+	    for i in range(num_conv_layers):
+		    activation = 'relu' if i != num_conv_layers - 1 else None
+		    self.kernels.append(
+				    tf.keras.layers.Conv2d(filters[i], self.kernel_sizes[i], activation=activation) # padding is valid in training, but not when evaluating, keras layes may not be ideal for this
+
+	    assert(len(self.kernels) == self.num_conv_layers)
         
     def call(self, images):
 	    
 	    # should convolving with kernel happen here or in loss, my intuition is in loss
-        pass
+	    out = images
+	    for i in range(self.num_conv_layers):
+	    	out = self.kernels(out)
+	return out
 
     def loss(self, denoised, original):
         """
