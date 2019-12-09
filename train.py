0b48d9225f424de7eff9c010afd0916d20b0509e
AndrewCanino <45506494+AndrewCanino@users.noreply.github.com>
2019-12-08 21:27:13 -0500

    Added EPSILON and fixed train to match model
    Not sure how to get albedo, don't know if anybody else knows more about this, but we can definitely do the post processing here.

--- train.py
+++ train.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--num_epoch', type=int, default=10 help='Number of epochs to run [default: 10]')
 parser.add_argument('--batch_size', type=int, default=5, help='Number of epochs to run [default: 32]')
 parser.add_argument('--learning_rate', type=int, default=0.0001, help='Learning rate [default: e-5]')
+parser.add_argument('--epsilon', type=int, default=0.00316, help='Epsilon [default: 0.00316]')
 parser.add_argument('--log_dir', type=str, default='log', help='Log dir [default: log]')
 parser.add_argument('--dataset', type=str, default='images', help='Dataset path  [default: images]')
 FLAGS = parser.parse_args()
@@ -17,6 +18,7 @@ FLAGS = parser.parse_args()
 NUM_EPOCH = FLAGS.num_epoch
 BATCH_SIZE = FLAGS.batch_size
 LEARNING_RATE = FLAGS.learning_rate
+EPSILON = FLAGS.epsilon
 LOG_DIR = FLAGS.log_dir
 DATASET = FLAGS.dataset
 
@@ -34,7 +36,9 @@ def train():
 			batch_inputs = inputs[i:i+BATCH_SIZE]
 			batch_labels = labels[i:i+BATCH_SIZE]
 			with tf.GradientTape() as tape:
-				predictions = model(batch_inputs)
+				diffuse, specular = model.call(batch_inputs)
+                # predictions = (albedo + EPSILON) dot diffuse + exp(specular) - 1
+                predictions = EPSILON * diffuse + np.exp(specular) - 1
 				loss = model.loss(predictions, batch_labels)
 			gradients = tape.gradient(loss, model.trainable_variables)
 			optimizer.apply_gradients(zip(gradients, model.trainable_variables))
@@ -45,5 +49,4 @@ def train():
 
 if __name__ == '__main__':
 	train()
-
 
