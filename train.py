749a89356f3d53860cc55815705c163098b9e1e8
AndrewCanino <45506494+AndrewCanino@users.noreply.github.com>
2019-12-11 14:15:09 -0500

    Added patching

--- train.py
+++ train.py
@@ -9,7 +9,8 @@ from imageio import imwrite
 # will change hyperparameters later
 parser = argparse.ArgumentParser()
 parser.add_argument('--num_epoch', type=int, default=100, help='Number of epochs to run [default: 10]')
-parser.add_argument('--batch_size', type=int, default=5, help='Number of epochs to run [default: 32]')
+parser.add_argument('--batch_size', type=int, default=5, help='Batch size [default: 5]')
+parser.add_argument('--patch_size', type=int, default=100, help='Patch size [default: 100]')
 parser.add_argument('--learning_rate', type=int, default=0.0001, help='Learning rate [default: e-5]')
 parser.add_argument('--epsilon', type=int, default=0.00316, help='Epsilon [default: 0.00316]')
 parser.add_argument('--log_dir', type=str, default='log', help='Log dir [default: log]')
@@ -32,30 +33,26 @@ def train():
 	optimizer = tf.keras.optimizers.Adam(LEARNING_RATE)
 	#saver = tf.train.Saver()
 
+    
+	s = 'images/original_cornell_box.png'
+	imwrite(s, inputs)
+    
 	inputs = tf.reshape(inputs, (1, 512, 512, 3))/255
 	labels = tf.reshape(labels, (1, 512, 512, 3))/255
 	for epoch in range(NUM_EPOCH):
-		'''
-		for i in range(0, len(inputs) - BATCH_SIZE, BATCH_SIZE):
-			batch_inputs = inputs[i:i+BATCH_SIZE]
-			batch_labels = labels[i:i+BATCH_SIZE]
-			with tf.GradientTape() as tape:
-				diffuse, specular = model.call(batch_inputs, batch_inputs)
-                # predictions = (albedo + EPSILON) dot diffuse + exp(specular) - 1
-				predictions = EPSILON * diffuse #+ np.exp(specular) - 1
-				loss = model.loss(predictions, batch_labels)
-			gradients = tape.gradient(loss, model.trainable_variables)
-			optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-			if i % 10 == 0:
-				print("LOSS:", loss)
-		'''
-		with tf.GradientTape() as tape:
-			diffuse, specular = model.call(inputs, inputs)
-			predictions = EPSILON * diffuse
-			loss = model.loss(predictions, labels)
-		gradients = tape.gradient(loss, model.trainable_variables)
-		optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-		print("LOSS", epoch, ":", loss)
+        for i in range(0, len(inputs) - BATCH_SIZE, BATCH_SIZE):
+            for j in range(0, inputs.shape[1] - PATCH_SIZE, PATCH_SIZE):
+                for k in range(0, inputs.shape[2] - PATCH_SIZE, PATCH_SIZE):
+                    with tf.GradientTape() as tape:
+                        batch_patch_inputs = inputs[i:i+BATCH_SIZE][j:j+PATCH_SIZE][k:k+PATCH_SIZE]
+                        batch_patch_labels = labels[i:i+BATCH_SIZE][j:j+PATCH_SIZE][k:k+PATCH_SIZE]
+                        diffuse, specular = model.call(batch_patch_inputs, batch_patch_labels)
+                        predictions = EPSILON * diffuse + tf.exp(specular) - 1
+                        loss = model.loss(predictions, batch_patch_labels)
+                    gradients = tape.gradient(loss, model.trainable_variables)
+                    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+                    print("LOSS", epoch, ":", loss)
+                    
 
 	# dont quite remember how to save, need sessions?
 	# save_path = saver.save(, os.path.join(LOG_DIR, "model.ckpt"))
