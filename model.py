a55edc66dc442c6807bc67fcdd323c24cb82b6e6
AndrewCanino <45506494+AndrewCanino@users.noreply.github.com>
2019-12-11 14:16:29 -0500

    Uncommmented specular components

--- model.py
+++ model.py
@@ -27,17 +27,17 @@ class Denoise(tf.keras.Model):
         assert len(self.kernel_sizes) == self.num_conv_layers
 
         self.diffuse_kernels = []
-        #self.specular_kernels = []
+        self.specular_kernels = []
 
         for i in range(self.num_conv_layers):
             activation = 'relu' if i != self.num_conv_layers - 1 else None
             self.diffuse_kernels.append(tf.keras.layers.Conv2D(self.filters[i], self.kernel_sizes[i], padding='same', activation=activation)) # padding is valid in training, but not when evaluating, keras layers may not be ideal for this
-            #self.specular_kernels.append(tf.keras.layers.Conv2D(self.filters[i], self.kernel_sizes[i], padding='same', activation=activation))
+            self.specular_kernels.append(tf.keras.layers.Conv2D(self.filters[i], self.kernel_sizes[i], padding='same', activation=activation))
 
         self.diffuse_kernels.append(tf.keras.layers.Conv2D(3, 21, padding='same', activation=None))
-        #self.specular_kernels.append(tf.keras.layers.Conv2D(3, 21, padding='same', activation=None))
+        self.specular_kernels.append(tf.keras.layers.Conv2D(3, 21, padding='same', activation=None))
         assert len(self.diffuse_kernels) == self.num_conv_layers + 1
-        #assert len(self.specular_kernels) == self.num_conv_layers + 1
+        assert len(self.specular_kernels) == self.num_conv_layers + 1
 
     def call(self, diffuse_images, specular_images):
 
@@ -46,11 +46,11 @@ class Denoise(tf.keras.Model):
         spec_out = tf.dtypes.cast(specular_images, dtype=tf.float32)
         for i in range(self.num_conv_layers + 1):
             diff_out = self.diffuse_kernels[i](diff_out)
-            #spec_out = self.specular_kernels[i](spec_out)
+            spec_out = self.specular_kernels[i](spec_out)
         return diff_out, spec_out
-
+    
     def loss(self, denoised, original):
-
+        
         """
         Computes the loss for a batch of images.
         :param denoised: a 4d matrix (batchsize, width, height, 3) of pixel values representing a batch of image, the output of call
@@ -61,10 +61,10 @@ class Denoise(tf.keras.Model):
         original = tf.dtypes.cast(original, tf.float32)
 
     	# model.call() here
-
+        
         return tf.reduce_sum(tf.abs(denoised - original))
 
-
+    
     def accuracy(self, denoised, original):
         """
         Computes the accuracy for a batch of images.
@@ -74,9 +74,9 @@ class Denoise(tf.keras.Model):
         """
         denoised = tf.dtypes.cast(denoised, tf.float32)
         original = tf.dtypes.cast(original, tf.float32)
-
+        
         '''
         for each image in the batch, average the differences between each color channel
-        then average these across each pixel of each image
+        then average these across each pixel of each image 
         '''
         return tf.reduce_mean(tf.reduce_mean(tf.abs(denoised - original), 3))
